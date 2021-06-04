5fe3e86139b31190c7f51c8493170c37287f187d
surya7765 <47697392+surya7765@users.noreply.github.com>
2021-06-04 21:05:44 +0530

    Update settings.py

--- ipproject/settings.py
+++ ipproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9p9s!20*i5_eqmji%@t6=-s-s@7n$bwox*-goo0hy8=tc*-9-y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['velipproject.herokuapp.com']
 
 
 # Application definition
